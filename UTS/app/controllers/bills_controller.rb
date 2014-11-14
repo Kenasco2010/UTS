06820a89f7eb455df3552d0c5cfa14ae797bd318
Kennedy <kennedy.anyinatoe@meltwater.org>
2014-11-14 02:37:00 +0000

    done with create,delete,show, working on styling and correcting errors on update function

--- UTS/app/controllers/bills_controller.rb
+++ UTS/app/controllers/bills_controller.rb
@@ -1,29 +1,44 @@
 class BillsController < ApplicationController
 	layout 'Main'
 	def index
-		@bills = Bill.all
+		@bill = Bill.all
 	end
-
+	def new
+		@bill = Bill.new
+	end
 	def create
 		@bill = Bill.new(bill_params)
 
 		if @bill.save
-		redirect_to @bill
+			redirect_to @bill
 		else
-		render 'new'
+			render 'new'
 		end
 	end
 
-	def new
+	def update
+		@bill = Bill.find(params[:id])
 
+		if @bill.update(bill_params)
+			flash[:notice] = 'Profile was successfully updated.'
+			redirect_to @bill
+		else
+			render 'edit'
+		end
 	end
 
 	def edit
-		
+		@bill = Bill.find(params[:id])
 	end
 
 	def show
 		@bill = Bill.find(params[:id])
+	end
+	def destroy
+		@bill = Bill.find(params[:id])
+		@bill.destroy
+		
+		redirect_to bills_path
 	end
 
 end
